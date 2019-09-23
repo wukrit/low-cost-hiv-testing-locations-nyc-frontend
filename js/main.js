9fe9d6a2df615e152c32dc9d5e105e5455f94e50
wukrit <sukritwalia@gmail.com>
2019-09-23 13:30:28 -0400

    Added modals

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // Fetch to NYC Open Data
 const testingLocationsUrl = "https://data.cityofnewyork.us/resource/72ss-25qh.json"
+
+// Initial Fetch
 fetch(testingLocationsUrl)
 	.then(response => response.json())
 	.then(locationArr => {
@@ -10,6 +12,13 @@ fetch(testingLocationsUrl)
 		})
 	})
 
+// Specific Fetch
+const fetchLocation = siteId => {
+	return obj = fetch(`${testingLocationsUrl}?site_id=${siteId}`)
+		.then(response => response.json())
+		.then(locationArr => locationArr[0])
+}
+
 // DOM Helper Methods
 const propertyIsDefined = (obj, property) => {
 	if (typeof obj[property] === "undefined") {
@@ -17,7 +26,7 @@ const propertyIsDefined = (obj, property) => {
 	} else {
 		return obj[property]
 	}
-} 
+}
 
 const createLocationTr = locationObj => {
 	const locationTr = document.createElement("tr")
@@ -30,6 +39,9 @@ const createLocationTr = locationObj => {
 	const free = document.createElement("td")
 
 	locationTr.id = `testing-location-id-${locationObj.site_id}`
+	locationTr.addEventListener("click", handleTrClick)
+
+
 	siteName.classList.add("site-name")
 	sitePhoneNumber.classList.add("site-phone")
 	siteAddress1.classList.add("site-address")
@@ -48,4 +60,54 @@ const createLocationTr = locationObj => {
 
 	locationTr.append(siteName, sitePhoneNumber, free, siteAddress1,  siteAddress2, siteZip, borough)
 	return locationTr
+}
+
+
+const handleTrClick = (e) => {
+	let locationId = ""
+	if (e.target.tagName !== "TR" ) {
+		locationId = e.target.parentElement.id.substr(20)
+		console.log(locationId);
+	} else {
+		locationId = e.target.id.substr(20)
+		console.log(locationId);
+	}
+
+	const promise = fetchLocation(locationId)
+	createLocationModal(promise)
+}
+
+const createLocationModal = (promise) => {
+	const locationModal = document.createElement("div")
+	locationModal.classList.add("modal")
+	const modalContent = document.createElement("div")
+	modalContent.classList.add("modal-content")
+
+	const siteName = document.createElement("h2")
+	const sitePhoneNumber = document.createElement("p")
+	const siteAddress1 = document.createElement("p")
+	const siteAddress2 = document.createElement("p")
+	const siteZip = document.createElement("p")
+	const borough = document.createElement("p")
+	const free = document.createElement("p")
+
+	promise.then(locationObj => {
+		siteName.innerText = propertyIsDefined(locationObj, "site_name")
+		sitePhoneNumber.innerText = `Phone Number: ${propertyIsDefined(locationObj, "phone_number")}`
+		siteAddress1.innerText = `Address: ${propertyIsDefined(locationObj, "address")}`
+		siteAddress2.innerText = `Building/Office: ${propertyIsDefined(locationObj, "building_floor_suite")}`
+		siteZip.innerText = `Zip Code: ${propertyIsDefined(locationObj, "zip_code")}`
+		borough.innerText = `Borough: ${propertyIsDefined(locationObj, "borough")}`
+		free.innerText = `Free Testing Offered: ${propertyIsDefined(locationObj, "free")}`
+
+		modalContent.append(siteName, sitePhoneNumber, free, siteAddress1,  siteAddress2, siteZip, borough)
+		locationModal.append(modalContent)
+
+		document.body.append(locationModal)
+		window.addEventListener("click", (event) => {
+		  if (event.target === locationModal) {
+		    locationModal.remove()
+		  }
+		})
+	})
 }
